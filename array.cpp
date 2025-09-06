f9f374d95221122be7456bcebfd7cc46f06fe896
Akarsh-cy <cygnusd6@gmail.com>
2025-09-06 21:20:24 +0530

    something

--- array.cpp
+++ array.cpp
@@ -10,19 +10,32 @@ for(std::size_t i=std::size(v);i>0;i--){
    }
    }//fn ends
 
-vector<float> reverse(vector<float> v){
-      vector<float> s(v.size());
-      for(int i =v.size();i>0;i--){
-         s[v.size()-i]=v[i-1];
-      }
-      return s;
+template<typename T>
+   vector<T> reverse(vector<T>& v){
+   const std::size_t size{std::size(v)};
+   std::cout<<size;
+   vector<auto> c(size,0)
+   for(std::size_t i=0;i<size;i++){
+   c[i]=v[size-i];
+   }//for ends
+
+
+
    }//fn ends
+
+
+
+
+
+
+
 
 
 }//namespace ends
 int main(){
 vector v{1,2,3,4,5,6,7,8,9,10};
 //Custom::display_reverse(v);
-Custom::reverse(v);
+ vector s{Custom::reverse(v)};
+ std::cout<<v<<"nigger\n"<<s;
 
 }
