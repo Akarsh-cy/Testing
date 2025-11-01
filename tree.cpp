f95ec2df7b4404257fba4c3c92005860fa8fffdf
Akarsh-cy <cygnusd6@gmail.com>
2025-11-01 11:26:10 +0530

    added tree traversal for parent with n childs {inefficent version}

--- tree.cpp
+++ tree.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
+#include<vector>
+
+class Parent{
+    public:
+        int value;
+        std::vector<Parent*> child;
+    Parent(int val):value{val}{
+for(auto thing:child){thing=nullptr;}
+    }
+
+    void join(Parent* obj){
+    child.push_back(obj);
+    }
+
+
+};
+
+void printTree(Parent* root){
+if(root==nullptr){return;}
+
+std::cout<<" ";
+std::cout<<root->value;
+for(auto item:root->child){
+printTree(item);
+}
+
+}
+
+
+
 
 class Node{
-
 public:
   int data;
   Node* left;
@@ -13,14 +42,22 @@ public:
   }
 };//class ends
 
-void printTree(Node* root){
+void printBtree(Node* root){
 if(root==nullptr)return;
 
 std::cout<<root->data;
-printTree(root->left);
-printTree(root->right);
+printBtree(root->left);
+printBtree(root->right);
 }
 
+std::vector<Parent> make(int n){
+std::vector<Parent> obj;
+obj.reserve(n);
+for(int i=0;i<n;++i){
+obj.push_back(Parent(i+1));
+}
+return obj;
+}
 
 int main(){
 Node root=Node(6);
@@ -30,5 +67,27 @@ Node r=Node(8);
 root.left=&l;
 root.right=&r;
 
-printTree(&root);
+//printBtree(&root);
+
+//general tree
+std::vector<Parent> vec=make(8);
+
+vec[0].join(&vec[1]);
+vec[0].join(&vec[2]);
+
+vec[1].join(&vec[3]);
+
+vec[1].join(&vec[4]);
+
+vec[1].join(&vec[5]);
+
+vec[2].join(&vec[6]);
+vec[2].join(&vec[7]);
+
+printTree(&vec[0]);
+
+
+
+
+
 }
