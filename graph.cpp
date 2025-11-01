d3092dd352489d911b5661454be946978bf3834d
Akarsh-cy <cygnusd6@gmail.com>
2025-11-01 20:54:43 +0530

    graph test

--- graph.cpp
+++ graph.cpp
@@ -1,99 +1,87 @@
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <iostream>
 
-class Node{
-    public:
-        int value;
-        std::vector<Node*> edge;
+class Node {
+public:
+    int value;
+    std::vector<Node*> edge;
 
-    Node(int val):value{val}{
-      for(auto item: edge)
-      item=nullptr;
+    Node(int val = 0) : value{val} {}
+
+    void join(Node* a) {
+        edge.push_back(a);
     }
+};
 
-    void join(Node*a,int index){
-this->edge[index]=a;
-    }
-
-};//class ends
-
-
-
-std::vector<Node> make(int size){
+std::vector<Node> make(int size) {
     std::vector<Node> obj;
-    obj.reserve(size);
-    for(int i=0;i<size;++i)
-        obj[i]=Node(i+1);
+    for (int i = 0; i < size; ++i)
+        obj.emplace_back(Node(i+1));
     return obj;
 }
 
-void printGraph(Node* root){
+std::vector<int> dfs_find(Node* root, std::vector<bool>& visited,int& target,std::vector<int>& path){
 
-if(root==nullptr){return;}
+if(root==nullptr && !path.empty()){
+path.pop_back();
+}
 
-std::cout<<root->value<<" ";
+path.push_back(root->value);
+
+if(path[path.size()-1]==target)return path;
+
+if(root->value>visited.size()){
+visited.resize(2*root->value);
+visited[root->value]=true;
+}
+
+if(root->value<=visited.size()){
+
+    if(!visited[root->value]){
 for(auto item:root->edge){
-printGraph(item);
-
-}//for loop ends
-
+dfs_find(item,visited,target,path);
+}
+visited[root->value]=true;
+ }
 }
 
 
+}
+ 
 
 
-std::vector<int> solver(Node* start){
+
+
+
+void dfs(Node* start, std::vector<int>& path) {
+    if (start==nullptr) return;
+    path.push_back(start->value);
+    for (auto item : start->edge)
+        dfs(item, path);
+}
+
+int main() {
+    auto i = make(12);
+
+    i[0].join(&i[3]);
+    i[0].join(&i[1]);
+    i[1].join(&i[2]);
+    i[1].join(&i[4]);
+    i[3].join(&i[7]);
+    i[7].join(&i[8]);
+    i[8].join(&i[9]);
+    i[9].join(&i[10]);
+    i[10].join(&i[11]);
+    i[4].join(&i[6]);
+    i[4].join(&i[5]);
+
+    i.push_back(Node(13));
+    i[11].join(&i[12]);
+
     std::vector<int> sol;
-    sol.reserve(10);
+    dfs(&i[0], sol);
 
-if(start==nullptr){
-if(sol.at(0)==sol.at(sol.size()-1))
-return sol;
-}
-
-sol.push_back(start->value);
-
-for(auto item:start->edge){
-    sol=solver(item);
-    sol.erase(sol.begin()+1,sol.end());
-}
-std::vector<int> noans(0);
-
-return noans;
-}
-
-int main(){
-auto i=make(12);
-
-i[0].join(&i[3],0);
-i[0].join(&i[1],1);
-i[1].join(&i[2],0);
-
-i[1].join(&i[4],1);
-i[3].join(&i[7],0);
-i[7].join(&i[8],0);
-i[8].join(&i[9],0);
-i[9].join(&i[10],0);
-i[10].join(&i[11],0);
-
-i[4].join(&i[6],0);
-i[4].join(&i[5],0);
-
-i.push_back(Node(1));
-i[11].join(&i[12],0);
-
-
-auto sol=solver(&i[0]);
-for(auto item:sol){
-    std::cout<<item<<" ";
-
-}
-std::cout<<std::endl;
-
-
-
-
-
-
-
+    for (auto item : sol)
+        std::cout << item << " ";
+    std::cout << std::endl;
 }
